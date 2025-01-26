92f811395911d62b9a35b159cc3f2e5e11c23189
Jan <138018029+IxI-Enki@users.noreply.github.com>
2025-01-26 11:25:22 +0100

    Update advent-of-code-001.py

--- advent-of-code-001.py
+++ advent-of-code-001.py
@@ -1,25 +1,48 @@
 # __ IMPORTED MODULES : ____________________________________________________
-import keyboard
-import random
-import time
 import os
+import time
 #==============================================================================
 
 # __ GLOBALS : _____________________________________________________________
-test_file_path = 'resources/test-file.txt'
-file_path = 'resources/data-day-two.txt'
+file_path = 'resources/day1.txt'
 #==============================================================================
 
-def read_file(path):
-  file = open(path, 'r')
+def read_file():
+  file = open(file_path, 'r')
   f = file.readlines()
   file.close()
   return f
 
 
-def main():
-  f = read_file(test_file_path) 
-  print(f)
+def main(): 
+  first_column = []
+  second_column = []
+
+  f = read_file();
+
+  for line in f:
+      splitLine = line.split('   ')
+      first_column.append(splitLine[0].strip())
+      second_column.append(splitLine[1].strip())
+  
+  first_column.sort()
+  second_column.sort()
+  
+  print('first 8 elements first column: ', first_column[:8])
+  print('first 8 elements second column: ', second_column[:8])
+  
+  # distance = sum([abs(int(a), int(b)) for a,b in zip(first_column, second_column)])
+
+  first_column = list(map(int, first_column))
+  second_column = list(map(int, second_column))
+  total_sum = 0
+   
+  for i in range(len(first_column)):
+      currentCount = second_column.count(first_column[i])
+      total_sum += first_column[i] * currentCount
+      
+  #print(distance)
+  print(total_sum)
 
 
 #____CALL_MAIN_PROGRAM_________________
